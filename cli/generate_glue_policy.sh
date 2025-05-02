b31218f2377c6089c003126eb8ffb6ba1827e2ef
davlaj <168560449+davlaj@users.noreply.github.com>
2025-05-02 14:07:05 -0400

    Update generate_glue_policy.sh

--- cli/generate_glue_policy.sh
+++ cli/generate_glue_policy.sh
@@ -2,13 +2,16 @@
 
 RAW_BUCKET="lab1-raw-data-david"
 PROCESSED_BUCKET="lab1-processed-data-david"
-KMS_ARN="arn:aws:kms:us-east-1:123456789012:key/your-key-id"  # Replace after key creation
+KEY_ID=$(cat ../kms/last-created-key-id.txt)
+AWS_REGION="us-east-1"
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+KMS_ARN="arn:aws:kms:$AWS_REGION:$AWS_ACCOUNT_ID:key/$KEY_ID"
 
-# Substitute variables in template
+# Substitute into template
 sed \
   -e "s|{{RAW_BUCKET}}|$RAW_BUCKET|g" \
   -e "s|{{PROCESSED_BUCKET}}|$PROCESSED_BUCKET|g" \
   -e "s|{{KMS_ARN}}|$KMS_ARN|g" \
   ../iam/glue-role-policy.template.json > ../iam/glue-role-policy.final.json
 
-echo "Generated final policy: ../iam/glue-role-policy.final.json"
+echo "Generated glue-role-policy.final.json using KMS Key $KEY_ID"
