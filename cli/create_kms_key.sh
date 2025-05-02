5adbccf5756328f0030422ec17bb4348b25168c0
davlaj <168560449+davlaj@users.noreply.github.com>
2025-05-02 15:38:45 -0400

    Update create_kms_key.sh

--- cli/create_kms_key.sh
+++ cli/create_kms_key.sh
@@ -9,8 +9,12 @@ KEY_ID=$(aws kms create-key \
 
 echo "Created KMS Key: $KEY_ID"
 
+# Enable key rotation
 aws kms enable-key-rotation --key-id "$KEY_ID"
 echo "Key rotation enabled for $KEY_ID"
+
+# Create directory if it doesn't exist
+mkdir -p ../kms
 
 # Save key id to file
 echo "$KEY_ID" > ../kms/last-created-key-id.txt
